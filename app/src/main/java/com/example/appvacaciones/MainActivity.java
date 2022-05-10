9bdac83338b78bc9b7bcef83a54dd3348090eea0
Mario999X <80423737+Mario999X@users.noreply.github.com>
2022-05-10 14:51:11 +0200

    Eliminación de un sout

--- app/src/main/java/com/example/appvacaciones/MainActivity.java
+++ app/src/main/java/com/example/appvacaciones/MainActivity.java
@@ -93,7 +93,6 @@ public class MainActivity extends AppCompatActivity {
             intent.putExtra("pais", casaList.get(posicion).getPais());
             intent.putExtra("precio", casaList.get(posicion).getPrecio());
             startActivity(intent);
-            System.out.println("funciona");
         };
     }
 
